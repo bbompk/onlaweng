b09308931a1e1b18f766eab0655fc5e84eca805b
bbompk <panithiv@gmail.com>
2023-05-14 16:21:55 +0700

    added mediapipe object detection

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # onlaweng
 household object detection by finger tips
 
-### docs
-Mediapipe: <br/>
-https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/python
-https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/index#models
+## docs
+### Mediapipe 
+Hand Detection: <br/>
+https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/python <br/>
+https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/index#models <br/>
+<br/>
+Object Detection: <br/>
+https://developers.google.com/mediapipe/solutions/vision/object_detector/python#configuration <br/>
+https://developers.google.com/mediapipe/solutions/vision/object_detector#models <br/>
--- onlaweng_utils.py
+++ onlaweng_utils.py
@@ -119,3 +119,23 @@ def draw_landmarks(
       # Fill color into the circle
       cv2.circle(image, landmark_px, drawing_spec.circle_radius,
                  drawing_spec.color, drawing_spec.thickness)
+      
+def draw_bounding_boxes(image, detection) :
+    bbox = detection.bounding_box
+    # print(bbox)
+    label = sorted([(c.category_name, round(c.score, 2)) for c in detection.categories])[0]
+    color = np.array(tuple(np.random.random(size=3) * 256))
+    cv2.rectangle(image, 
+                  np.array([bbox.origin_x, bbox.origin_y]), 
+                  np.array([bbox.origin_x + bbox.width, bbox.origin_y + bbox.height]),
+                  color=color,
+                  thickness=2)
+    
+    cv2.putText(image, 
+                str(f"{label[0]} {label[1]}"), 
+                org=np.array([bbox.origin_x, bbox.origin_y - 5]), 
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,      
+                fontScale=0.75, 
+                color=color, 
+                thickness=1, 
+                lineType=cv2.LINE_AA)
